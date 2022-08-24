39c50ee497b2850e1827967c4d0a8d08b60dd1b3
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-24 19:28:28 +0530

    minor changes

--- src/contracts/contracts/NFTMarketplace.sol
+++ src/contracts/contracts/NFTMarketplace.sol
@@ -1,7 +1,5 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- src/contracts/hardhat.config.js
+++ src/contracts/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-ethers");
+const fs = require("fs");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork : "hardhat",
+  networks : {
+    hardhat : {
+      chainId : 1337
+    },
+    goerli : {
+      url : "https://eth-goerli.g.alchemy.com/v2/g5kUI2wieOnZAKaZ78804HsmjGn86cVI",
+      accounts : ["271d2b0391f00e5257d63e080cb97cf0ac3ef6dde736dcb1fff811edbc142b89"]
+    }
+  },
   solidity: "0.8.9",
 };
