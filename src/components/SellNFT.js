29fd392a600c571470fc3fd30cb3d4744add0b0c
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-24 23:04:46 +0530

    Pages initialising

---
+++ src/components/SellNFT.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SellNFT = () => {
+  return (
+    <div>SellNFT</div>
+  )
+}
+
+export default SellNFT
---
+++ src/components/SingleNFT.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleNFT = () => {
+  return (
+    <div>SingleNFT</div>
+  )
+}
+
+export default SingleNFT
