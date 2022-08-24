621d35aa181cf1654ad6affc1a2eb1d1dfad4236
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-24 23:53:26 +0530

    dependency changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Marketplace from "./components/Marketplace";
+import Profile from "./components/Profile";
+import SellNFT from "./components/SellNFT";
+import NFTPage from "./components/NFTPage";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Marketplace />} />
+          <Route path="/nftPage" element={<NFTPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sellNFT" element={<SellNFT />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
