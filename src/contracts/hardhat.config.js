9d84faa4f3479b3d308a519dc4c302b7c600513f
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-24 20:45:44 +0530

    deploy script and config.js changes

--- src/contracts/hardhat.config.js
+++ src/contracts/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomicfoundation/hardhat-ethers");
+require("@nomiclabs/hardhat-ethers");
 const fs = require("fs");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
